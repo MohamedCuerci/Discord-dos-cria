9b860b36d5252dc4cc71f1f27cdc4f3db0e82d3e
Cuerci <lucas.cuerci25@gmail.com>
2022-06-21 12:26:00 -0300

    room model e page controller adicionado

--- app/controllers/turbo_devise_controller.rb
+++ app/controllers/turbo_devise_controller.rb
@@ -1,12 +1,12 @@
-class turboDeviseController < ApplicationController
+class TurboDeviseController < ApplicationController
   class Responder < ActionController::Responder
     def to_turbo_stream
       controller.render(options.merge(format: :html))
     rescue ActionView::MissingTemplate => error
       if get?
         raise error
-      elsif has_error? && default_action
-        render rendering_options.merge(format: :html, status: :unprocessable_entity)
+      elsif has_errors? && default_action
+        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
       else
         redirect_to navigation_location
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  scope :all_except, -> (user) { where.not(id: user)}
+  #isso vai listar todos os usuarios on, menos o usuario logado
 end
